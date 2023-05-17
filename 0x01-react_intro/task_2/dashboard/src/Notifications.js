fc728bb7694699b2393bbce44898f9f2805722a7
Yidu-xSkript <yidu95@gmail.com>
2023-05-17 19:01:31 +0300

    Copied react genereated files from task_1, installed npm packages, added icons, updated notification file and utils

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getLatestNotification } from "./utils";
+import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+
+function Notifications() {
+    function handleClose() {
+        console.log("Close button has been clicked")
+    }
+
+    return (
+        <div className="notifications">
+            <button
+                style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
+                aria-label="Close"
+                onClick={handleClose}
+            >
+                <img src={closeIcon} alt="closeIcon" width="10px" />
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data="default">New course available</li>
+                <li data="urgent">New resume available</li>
+                <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Notifications;
